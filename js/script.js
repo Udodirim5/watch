38bc547db5d8ebd37f9d7bbebfe490b90881e612
Udodirim <Udodirimwisdom@gmail.com>
2024-04-29 18:17:31 +0100

    an update

--- js/script.js
+++ js/script.js
@@ -71,6 +71,7 @@ document.getElementById("cam-btn").addEventListener("click", () => {
 document.getElementById("closeBtn").addEventListener("click", () => {
   camDiv.classList.remove("is-visible");
 });
+
 let capDisplay = document.getElementById("cam-display");
 let capInput = document.getElementById("file-upload");
 capInput.addEventListener("change", function () {
@@ -78,6 +79,22 @@ capInput.addEventListener("change", function () {
   capDisplay.src = URL.createObjectURL(capInput.files[0]);
 });
 
+  function startRealTimeClock() {
+    setInterval(function() {
+      let now = new Date();
+      let hours = now.getHours();
+      let minutes = now.getMinutes();
+      let ampm = hours >= 12 ? 'PM' : 'AM';
+      hours = hours % 12;
+      hours = hours ? hours : 12;
+      minutes = minutes < 10 ? '0' + minutes : minutes;
+      let strTime = hours + ':' + minutes + ' ' + ampm;
+      document.getElementById('curr-time').innerHTML = strTime;
+    }, 1000);
+  }
+  
+  // Call this function when your page loads or when you want to start the clock
+  startRealTimeClock();
 /*
 let interval;
 
